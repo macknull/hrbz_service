d7745ca7c94ff821418e980e17e12c59a84a7e41
Molski <macknull@gmail.com>
2019-11-01 13:06:08 +0100

    Initial commit for HRBZ REST Service

---
+++ src/main/java/hrbz_service/HrbzServiceApplication.java
@@ -0,0 +1,12 @@
+package hrbz_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrbzServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrbzServiceApplication.class, args);
+    }
+}
